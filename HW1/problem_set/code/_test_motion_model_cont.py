8d5337182fb7f5e02bb493ecba940529bbd4aaac
shahram95 <shahramnajam.neduet@gmail.com>
2024-09-21 19:43:31 -0400

    Adding structure for testing motion model for continuous paths

---
+++ HW1/problem_set/code/_test_motion_model_cont.py
@@ -0,0 +1,22 @@
+# Add relevant libraries
+
+def generate_circular_path():
+    pass
+
+def generate_straight_line_path():
+    pass
+
+def generate_figure_eight_path():
+    pass
+
+def generate_square_path():
+    pass
+
+def calculate_heading(path):
+    pass
+
+def test_motion_model():
+    pass
+
+if __name__ == "main":
+    pass
