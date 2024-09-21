15aaf4cf0ef561a6fe5df95e26592d161ba19f83
shahram95 <shahramnajam.neduet@gmail.com>
2024-09-21 19:53:08 -0400

    Added circular path generation function

--- HW1/problem_set/code/_test_motion_model_cont.py
+++ HW1/problem_set/code/_test_motion_model_cont.py
@@ -1,7 +1,11 @@
 # Add relevant libraries
+import numpy as np
 
-def generate_circular_path():
-    pass
+def generate_circular_path(center, radius, num_points):
+    theta = np.linspace(0, 2*np.pi, num_points)
+    x = center[0] + radius * np.cos(theta)
+    y = center[1] + radius * np.sin(theta)
+    return np.column_stack((x, y))
 
 def generate_straight_line_path():
     pass
@@ -17,6 +21,7 @@ def calculate_heading(path):
 
 def test_motion_model():
     pass
+
 
 if __name__ == "main":
     pass
