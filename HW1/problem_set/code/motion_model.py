5f3bf145f35c449864212d41521e1b814335b780
shahram95 <shahramnajam.neduet@gmail.com>
2024-09-21 23:13:48 -0400

    Optimized update function; corrected sample(x) substraction (rather than addition)

--- HW1/problem_set/code/motion_model.py
+++ HW1/problem_set/code/motion_model.py
@@ -19,10 +19,10 @@ class MotionModel:
         TODO : Tune Motion Model parameters here
         The original numbers are for reference but HAVE TO be tuned.
         """
-        self._alpha1 = 0.01
-        self._alpha2 = 0.01
-        self._alpha3 = 0.01
-        self._alpha4 = 0.01
+        self._alpha1 = 0.00001
+        self._alpha2 = 0.00001
+        self._alpha3 = 0.001
+        self._alpha4 = 0.001
     
     def normalize_angle(self, angle):
         return (angle + np.pi) % (2 * np.pi) - np.pi
@@ -47,20 +47,61 @@ class MotionModel:
         rot1 = self.normalize_angle(rot1)
         rot2 = self.normalize_angle(rot2)
 
-        # Add noise
-
+        # Add noise; sqrt because np.random.normal doesnot take variance instead require standard deviation
         noise_rot1 = np.sqrt(self._alpha1 * rot1**2 + self._alpha2 * trans**2)
         noise_trans = np.sqrt(self._alpha3 * trans**2 + self._alpha4 * (rot1**2 + rot2**2))
         noise_rot2 = np.sqrt(self._alpha1 * rot2**2 + self._alpha2 * trans**2)
 
-        rot1_with_noise = rot1 + np.random.normal(0, noise_rot1)
-        trans_with_noise = trans + np.random.normal(0, noise_trans)
-        rot2_with_noise = rot2 + np.random.normal(0, noise_rot2)
+        rot1_with_noise = rot1 - np.random.normal(0, noise_rot1)
+        trans_with_noise = trans - np.random.normal(0, noise_trans)
+        rot2_with_noise = rot2 - np.random.normal(0, noise_rot2)
 
         # Calculate new state
         x_t1 = np.zeros(3)
         x_t1[0] = x_t0[0] + trans_with_noise * np.cos(x_t0[2] + rot1_with_noise)
         x_t1[1] = x_t0[1] + trans_with_noise * np.sin(x_t0[2] + rot1_with_noise)
         x_t1[2] = self.normalize_angle(x_t0[2] + rot1_with_noise + rot2_with_noise)
+        
+        return x_t1
+
+    def update_optimized(self, u_t0, u_t1, x_t0):
+        '''
+        Optimized update method using vectorized operations.
+
+        Args:
+            u_t0 (np.ndarray): Particle state odometry reading [x, y, theta] at time (t-1) [odometry frame]
+            u_t1 (np.ndarray): Particle state odometry reading [x, y, theta] at time (t) [odometry frame]
+            x_t0 (np.ndarray): Particle state belief [x, y, theta] at time (t-1) [world frame]
+        
+            Returns:
+                np.ndarray: Updated particle state belief [x, y, theta] at time t [world frame]
+        '''
+        # calculate relative motion
+        delta_x, delta_y = u_t1[:2] - u_t0[:2]
+        rot1 = np.arctan2(delta_y, delta_x) - u_t0[2]
+        trans = np.hypot(delta_x, delta_y)
+        rot2 = u_t1[2] - u_t0[2] - rot1
+
+        # Normalize angle between -pi and pi to handle angle overflow
+        rot1, rot2 = map(self.normalize_angle, (rot1, rot2))
+
+        # Add noise; sqrt because np.random.normal doesnot take variance instead require standard deviation
+        noise_rot1 = np.sqrt(self._alpha1 * rot1**2 + self._alpha2 * trans**2)
+        noise_trans = np.sqrt(self._alpha3 * trans**2 + self._alpha4 * (rot1**2 + rot2**2))
+        noise_rot2 = np.sqrt(self._alpha1 * rot2**2 + self._alpha2 * trans**2)
+
+        rot1_with_noise = rot1 - np.random.normal(0, noise_rot1)
+        trans_with_noise = trans - np.random.normal(0, noise_trans)
+        rot2_with_noise = rot2 - np.random.normal(0, noise_rot2)
+
+        # Calculate new state
+        cos_term = np.cos(x_t0[2] + rot1_with_noise)
+        sin_term = np.sin(x_t0[2] + rot1_with_noise)
+
+        x_t1 = np.array([
+            x_t0[0] + trans_with_noise * cos_term,
+            x_t0[1] + trans_with_noise * sin_term,
+            self.normalize_angle(x_t0[2] + rot1_with_noise + rot2_with_noise)
+        ])
 
         return x_t1
