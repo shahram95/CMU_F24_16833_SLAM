cb1141e076f8a4ed10a647164ed86c05370ca7eb
shahram95 <shahramnajam.neduet@gmail.com>
2024-10-19 05:54:46 -0400

    Added test cases for solve_lu

--- HW3/problem_set/problem_set/code/test_solvers.py
+++ HW3/problem_set/problem_set/code/test_solvers.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.sparse import csr_matrix
 import unittest
-from solvers import solve_pinv
+from solvers import solve_pinv, solve_lu
 
 class TestSolvers(unittest.TestCase):
     def setUp(self):
@@ -13,6 +13,13 @@ class TestSolvers(unittest.TestCase):
         result = np.allclose(self.A @ x, self.b, atol=1e-6)
         print(f"solve_pinv test {'passed' if result else 'failed'}")
         self.assertTrue(result)
+    
+    def test_solve_lu(self):
+        x, U = solve_lu(self.A, self.b)
+        result1 = np.allclose(self.A @ x, self.b, atol=1e-6)
+        result2 = U.shape[0] == U.shape[1]  # Check if U is square
+        print(f"solve_lu test {'passed' if result1 and result2 else 'failed'}")
+        self.assertTrue(result1 and result2)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
